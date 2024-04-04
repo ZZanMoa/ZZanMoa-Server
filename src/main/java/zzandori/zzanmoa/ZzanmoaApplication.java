755785398fc46ec05ddc48682a794273cc2aa565
Yoo <hiyoojeong@gmail.com>
2024-04-04 17:43:02 +0900

    :tada: init: 프로젝트 생성

---
+++ src/main/java/zzandori/zzanmoa/ZzanmoaApplication.java
@@ -0,0 +1,13 @@
+package zzandori.zzanmoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzanmoaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZzanmoaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/zzandori/zzanmoa/ZzanmoaApplicationTests.java
@@ -0,0 +1,13 @@
+package zzandori.zzanmoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzanmoaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
