6f1ed46d3d647241bad114bcd9bbb5741df53492
ssuzyn <asj7090@gmail.com>
2024-04-09 21:58:33 +0900

    :sparkles: feat: 시장 데이터 응답 dto 생성

---
+++ src/main/java/zzandori/zzanmoa/market/dto/MarketResponseDto.java
@@ -0,0 +1,23 @@
+package zzandori.zzanmoa.market.dto;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import lombok.ToString;
+
+
+@ToString
+public class MarketResponseDto {
+
+    private Map<String, Set<String>> marketMap;
+
+    public MarketResponseDto() {
+        this.marketMap = new HashMap<>();
+    }
+
+    public void addMarket(String district, String marketName) {
+        this.marketMap.computeIfAbsent(district, k -> new HashSet<>()).add(marketName);
+    }
+
+}
