274a0a16e02c63fdbe3f6f066882b2e87005cc76
ssuzyn <asj7090@gmail.com>
2024-04-11 01:03:42 +0900

    :recycle: refactor: Open API 호출 메소드 분리

--- src/main/java/zzandori/zzanmoa/market/controller/MarketController.java
+++ src/main/java/zzandori/zzanmoa/market/controller/MarketController.java
@@ -3,9 +3,9 @@ package zzandori.zzanmoa.market.controller;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import zzandori.zzanmoa.market.dto.MarketResponseDto;
 import zzandori.zzanmoa.market.service.MarketService;
 
 @RestController
@@ -16,7 +16,14 @@ public class MarketController {
     private final MarketService marketService;
 
     @GetMapping("/get")
-    public MarketResponseDto getMarkets() throws IOException {
-        return marketService.getMarkets();
+    public void getMarkets() throws IOException {
+        marketService.getMarkets();
+    }
+
+    @GetMapping("/connectAPI/{startIndex}/{endIndex}")
+    public void connectOpenAPI(@PathVariable("startIndex") String startIndex,
+        @PathVariable("endIndex") String endIndex) throws IOException {
+
+        marketService.connectOpenAPI(startIndex, endIndex);
     }
 }
