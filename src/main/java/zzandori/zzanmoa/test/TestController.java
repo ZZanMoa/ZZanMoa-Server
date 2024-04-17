ebc838ffe85a05d8b33f7e2e2279c7984a8466e5
Yoo <hiyoojeong@gmail.com>
2024-04-17 19:18:18 +0900

    :rocket: add: Swagger UI 사용을 위한 Tagging

--- src/main/java/zzandori/zzanmoa/test/TestController.java
+++ src/main/java/zzandori/zzanmoa/test/TestController.java
@@ -1,8 +1,12 @@
 package zzandori.zzanmoa.test;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "TestController", description = "테스트를 위한 컨트롤러")
+@RequiredArgsConstructor
 @RestController
 public class TestController {
 
