932fa18ff1be76da6c768d3dbc23f8c23be278a9
ssuzyn <asj7090@gmail.com>
2024-04-24 22:18:21 +0900

    :sparkles: add: 할인행사 데이터 저장 전 디코딩 추가

--- src/main/java/zzandori/zzanmoa/openapi/config/JsonConfig.java
+++ src/main/java/zzandori/zzanmoa/openapi/config/JsonConfig.java
@@ -1,6 +1,8 @@
 package zzandori.zzanmoa.openapi.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +11,11 @@ public class JsonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
     }
 
 }
