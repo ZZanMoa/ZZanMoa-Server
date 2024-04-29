1dc362361cb63f8f4e5238161e45ba483b35fcef
ssuzyn <asj7090@gmail.com>
2024-04-30 01:16:48 +0900

    :sparkles: add: 이메일 정보 저장시 예외처리 추가

--- src/main/java/zzandori/zzanmoa/subscription/service/SubscriptionService.java
+++ src/main/java/zzandori/zzanmoa/subscription/service/SubscriptionService.java
@@ -2,6 +2,7 @@ package zzandori.zzanmoa.subscription.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import zzandori.zzanmoa.bargainboard.entity.District;
 import zzandori.zzanmoa.bargainboard.repository.DistrictRepository;
 import zzandori.zzanmoa.subscription.dto.SubscriptionDTO;
@@ -16,20 +17,26 @@ public class SubscriptionService {
     private final DistrictRepository districtRepository;
     private final SubscriptionRepository subscriptionRepository;
 
+    @Transactional
     public SubscriptionStatusDTO subscribe(SubscriptionDTO subscriptionDto) {
+        boolean allSavedSuccessfully = true;
+
         for (String districtName : subscriptionDto.getDistrict()) {
-            District district = districtRepository.findByDistrictName(districtName);
+            try {
+                District district = districtRepository.findByDistrictName(districtName);
+                Subscription subscription = Subscription.builder()
+                    .email(subscriptionDto.getEmail())
+                    .district(district)
+                    .build();
+                subscriptionRepository.save(subscription);
 
-            Subscription subscription = Subscription.builder()
-                .email(subscriptionDto.getEmail())
-                .district(district)
-                .build();
-
-            subscriptionRepository.save(subscription);
+            } catch (Exception e) {
+                allSavedSuccessfully = false;
+            }
         }
 
         return SubscriptionStatusDTO.builder()
-            .status(true)
+            .status(allSavedSuccessfully)
             .build();
     }
 
