bd838f7e6204fe0084cb2ad43cc48521268b260a
ssuzyn <asj7090@gmail.com>
2024-04-29 21:39:16 +0900

    :sparkles: feat: https 전환을 위한 OpenAPI 설정

--- src/main/java/zzandori/zzanmoa/ZzanmoaApplication.java
+++ src/main/java/zzandori/zzanmoa/ZzanmoaApplication.java
@@ -1,8 +1,11 @@
 package zzandori.zzanmoa;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://3.39.103.75:8080", description = "Generated server url")})
 @SpringBootApplication
 public class ZzanmoaApplication {
 
