a242815bd74835facb9311776d40b601eeaf7973
ssuzyn <asj7090@gmail.com>
2024-05-07 02:11:12 +0900

    :sparkles: chore: Event 엔티티 생성에 따른 메소드 수정

--- src/main/java/zzandori/zzanmoa/bargainboard/repository/BargainBoardRepository.java
+++ src/main/java/zzandori/zzanmoa/bargainboard/repository/BargainBoardRepository.java
@@ -7,15 +7,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import zzandori.zzanmoa.bargainboard.entity.BargainBoard;
-import zzandori.zzanmoa.bargainboard.entity.District;
-import zzandori.zzanmoa.bargainboard.entity.Event;
 
 public interface BargainBoardRepository extends JpaRepository<BargainBoard, Long> {
-    Page<BargainBoard> findByEvent(Event event, Pageable pageable);
-    Page<BargainBoard> findByDistrictId(Integer districtId, Pageable pageable);
-    Page<BargainBoard> findByEventAndDistrictId(Event event, Integer districtId, Pageable pageable);
+    Page<BargainBoard> findByEventId(Integer eventId, Pageable pageable);
+    Page<BargainBoard> findByEventIdAndDistrictId(Integer eventId, Integer districtId, Pageable pageable);
 
-    Page<BargainBoard> findAll(Pageable pageable);
 
     @Query("SELECT count(b) FROM BargainBoard b WHERE b.createdAt >= :startDate")
     int countRecentNews(LocalDate startDate);
