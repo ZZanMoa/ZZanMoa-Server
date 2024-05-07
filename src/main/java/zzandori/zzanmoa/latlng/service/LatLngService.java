3400deae0470c44db6bb4548c64eeff279cdb834
Yoo <hiyoojeong@gmail.com>
2024-05-07 18:59:37 +0900

    :art: refactor: 서울시 자치구, 행정동에 대한 위도, 경도 DB 저장 로직 리팩토링

--- src/main/java/zzandori/zzanmoa/latlng/service/LatLngService.java
+++ src/main/java/zzandori/zzanmoa/latlng/service/LatLngService.java
@@ -12,6 +12,7 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import zzandori.zzanmoa.latlng.dto.LatLngResponse;
 import zzandori.zzanmoa.latlng.entity.LatLngDistrict;
 import zzandori.zzanmoa.latlng.entity.LatLngSubDistrict;
@@ -29,56 +30,49 @@ public class LatLngService {
         return null;
     }
 
+    @Transactional
     public void saveLatitudeAndLongitude() {
         String filePath = "src/main/resources/seoul_latitude_and_longitude.csv";
+        Map<String, List<LatLngSubDistrict>> districts = parseCsvFile(filePath);
+        persistData(districts);
+    }
 
+    private Map<String, List<LatLngSubDistrict>> parseCsvFile(String filePath) {
         Map<String, List<LatLngSubDistrict>> districts = new HashMap<>();
-
         try (InputStreamReader reader = new InputStreamReader(new FileInputStream(filePath),
             StandardCharsets.UTF_8);
             CSVParser csvParser = new CSVParser(reader,
                 CSVFormat.DEFAULT.withFirstRecordAsHeader())) {
             for (CSVRecord csvRecord : csvParser) {
-                String district = csvRecord.get(2);
-                String dong = csvRecord.get(3);
-                Double latitude = Double.valueOf(csvRecord.get(4));
-                Double longitude = Double.valueOf(csvRecord.get(5));
-
-                if (!districts.containsKey(district)) {
-                    districts.put(district, new ArrayList<>());
-                } else {
-                    districts.get(district).add(LatLngSubDistrict.builder()
-                        .dong(dong)
-                        .latitude(latitude)
-                        .longitude(longitude)
-                        .build());
-                }
+                String districtName = csvRecord.get(2);
+                LatLngSubDistrict subDistrict = LatLngSubDistrict.builder()
+                    .dong(csvRecord.get(3))
+                    .latitude(Double.parseDouble(csvRecord.get(4)))
+                    .longitude(Double.parseDouble(csvRecord.get(5)))
+                    .build();
+                districts.computeIfAbsent(districtName, k -> new ArrayList<>()).add(subDistrict);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return districts;
+    }
 
-        for (String district : districts.keySet()) {
-            LatLngDistrict latLngDistrict = LatLngDistrict.builder()
-                .district(district)
-                .build();
-            latLngDistrictRepository.save(latLngDistrict);
+    private void persistData(Map<String, List<LatLngSubDistrict>> districts) {
+        districts.forEach((districtName, subDistricts) -> {
+            LatLngDistrict district = LatLngDistrict.builder().district(districtName).build();
+            latLngDistrictRepository.save(district);
 
-            List<LatLngSubDistrict> subDistricts = districts.get(district);
-            for (LatLngSubDistrict subDistrict : subDistricts) {
-                LatLngSubDistrict latLngSubDistrict = LatLngSubDistrict.builder()
-                    .district(latLngDistrict)
-                    .dong(subDistrict.getDong())
-                    .latitude(subDistrict.getLatitude())
-                    .longitude(subDistrict.getLongitude())
+            subDistricts.forEach(subDistrictData -> {
+                LatLngSubDistrict subDistrict = LatLngSubDistrict.builder()
+                    .district(district)
+                    .dong(subDistrictData.getDong())
+                    .latitude(subDistrictData.getLatitude())
+                    .longitude(subDistrictData.getLongitude())
                     .build();
-
-                latLngDistrict.addSubDistrict(latLngSubDistrict);
-
-                latLngSubDistrictRepository.save(latLngSubDistrict);
-            }
-
-        }
+                latLngSubDistrictRepository.save(subDistrict);
+            });
+        });
     }
 
 }
