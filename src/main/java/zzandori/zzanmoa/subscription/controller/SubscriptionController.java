5f4bab95f1470a8faa8f72808816d75adb9a4dc5
ssuzyn <asj7090@gmail.com>
2024-05-13 01:56:19 +0900

    :sparkles: add: 구독하기 요청 정보 유효성 검증 기능 추가

--- src/main/java/zzandori/zzanmoa/subscription/controller/SubscriptionController.java
+++ src/main/java/zzandori/zzanmoa/subscription/controller/SubscriptionController.java
@@ -1,7 +1,10 @@
 package zzandori.zzanmoa.subscription.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +14,7 @@ import zzandori.zzanmoa.subscription.dto.SubscriptionDTO;
 import zzandori.zzanmoa.subscription.dto.SubscriptionStatusDTO;
 import zzandori.zzanmoa.subscription.service.EmailScheduler;
 import zzandori.zzanmoa.subscription.service.SubscriptionService;
+import zzandori.zzanmoa.subscription.service.ValidatorService;
 
 @Tag(name = "SubscriptionController", description = "서울시 할인 소식 구독하기 기능 컨트롤러")
 @RestController
@@ -18,12 +22,18 @@ import zzandori.zzanmoa.subscription.service.SubscriptionService;
 @RequestMapping("/subscription")
 public class SubscriptionController {
 
+    private final ValidatorService validatorService;
     private final SubscriptionService subscriptionService;
     private final EmailScheduler emailScheduler;
 
     @PostMapping
-    public SubscriptionStatusDTO subscribe(@RequestBody SubscriptionDTO subscriptionDto) {
-        return subscriptionService.subscribe(subscriptionDto);
+    public ResponseEntity<?> subscribe(@Valid @RequestBody SubscriptionDTO subscriptionDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return validatorService.processBindingResultErrors(bindingResult);
+        }
+
+        SubscriptionStatusDTO status = subscriptionService.subscribe(subscriptionDto);
+        return ResponseEntity.ok(status);
     }
 
     @GetMapping("/send-test")
--- src/main/java/zzandori/zzanmoa/subscription/dto/SubscriptionDTO.java
+++ src/main/java/zzandori/zzanmoa/subscription/dto/SubscriptionDTO.java
@@ -1,5 +1,8 @@
 package zzandori.zzanmoa.subscription.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +16,14 @@ import lombok.ToString;
 @Builder
 @ToString
 public class SubscriptionDTO {
+
+    @NotBlank(message = "이름 입력은 필수 입니다.")
     private String name;
+
+    @NotBlank(message = "이메일 입력은 필수 입니다.")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
     private String email;
+
+    @NotEmpty(message = "자치구 선택은 필수 입니다.")
     private List<String> district;
 }
