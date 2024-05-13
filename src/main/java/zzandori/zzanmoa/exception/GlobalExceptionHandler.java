8c68f58c186e970ea1b7258623707735e5c4f113
Yoo <hiyoojeong@gmail.com>
2024-05-13 18:47:40 +0900

    :sparkles: feat: 전역 예외처리 모듈 추가

---
+++ src/main/java/zzandori/zzanmoa/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package zzandori.zzanmoa.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import zzandori.zzanmoa.test.TestException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(TestException.class)
+    protected ResponseEntity<ErrorResponse> handleTestException(TestException ex) {
+        ErrorCode errorCode = ex.getErrorCode();
+        return handleExceptionInternal(errorCode);
+    }
+
+    private ResponseEntity<ErrorResponse> handleExceptionInternal(ErrorCode errorCode) {
+        return ResponseEntity
+            .status(errorCode.getHttpStatus().value())
+            .body(new ErrorResponse(errorCode));
+    }
+
+}
