d63083d84b74a336f5fecfb2c2f0ec2b222e00d9
ssuzyn <asj7090@gmail.com>
2024-05-14 13:31:14 +0900

    :art: chore: 예외 응답 형식 수정

--- src/main/java/zzandori/zzanmoa/exception/subscription/DistrictDuplicatedErrorResponse.java
+++ src/main/java/zzandori/zzanmoa/exception/subscription/DistrictDuplicatedErrorResponse.java
@@ -1,14 +1,15 @@
 package zzandori.zzanmoa.exception.subscription;
 
+import java.util.List;
 import lombok.Getter;
 import zzandori.zzanmoa.exception.ErrorCode;
 import zzandori.zzanmoa.exception.ErrorResponse;
 
 @Getter
 public class DistrictDuplicatedErrorResponse extends ErrorResponse {
-    private final String district;
+    private final List<String> district;
 
-    public DistrictDuplicatedErrorResponse(ErrorCode errorCode, String district) {
+    public DistrictDuplicatedErrorResponse(ErrorCode errorCode, List<String> district) {
         super(errorCode);
         this.district = district;
     }
