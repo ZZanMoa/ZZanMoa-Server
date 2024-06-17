f62bd9c6e6d5c5a80293ebdb0507a157ac5fa29f
Yoo <hiyoojeong@gmail.com>
2024-06-17 19:23:08 +0900

    :sparkles: feat: URL 인코딩 예외처리 구현

--- src/main/java/zzandori/zzanmoa/googleapi/dto/review/ReviewRequest.java
+++ src/main/java/zzandori/zzanmoa/googleapi/dto/review/ReviewRequest.java
@@ -5,6 +5,8 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import zzandori.zzanmoa.googleapi.exception.GoogleApiErrorCode;
+import zzandori.zzanmoa.googleapi.exception.GoogleApiException;
 
 @Configuration
 public class ReviewRequest {
@@ -14,8 +16,13 @@ public class ReviewRequest {
     @Value("${GOOGLE_MAP_API_KEY}")
     private String GOOGLE_MAP_API_KEY;
 
-    public String requestUrl(String placeId) throws UnsupportedEncodingException {
-        String encodedPlaceId = URLEncoder.encode(placeId, StandardCharsets.UTF_8.toString());
+    public String requestUrl(String placeId) {
+        String encodedPlaceId = null;
+        try {
+            encodedPlaceId = URLEncoder.encode(placeId, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            throw new GoogleApiException(GoogleApiErrorCode.UNSUPPORTED_ENCODING);
+        }
         StringBuilder urlBuilder = new StringBuilder(GOOGLE_MAP_API_REVIEW_BASE_URL)
             .append("?fields=").append("reviews")
             .append("&language=").append("ko")
