0b5a2359fd5f2c7c711e088479cb43b850d9d2dc
Yoo <hiyoojeong@gmail.com>
2024-06-17 18:32:07 +0900

    :sparkles: feat: 시장 비교하기 중 각각의 시장 리뷰를 조회하는 기능 구현

--- src/main/java/zzandori/zzanmoa/marketplace/service/MarketPlaceService.java
+++ src/main/java/zzandori/zzanmoa/marketplace/service/MarketPlaceService.java
@@ -2,17 +2,16 @@ package zzandori.zzanmoa.marketplace.service;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
-import java.util.function.Function;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import zzandori.zzanmoa.googleapi.dto.Candidate;
-import zzandori.zzanmoa.googleapi.dto.Location;
 import zzandori.zzanmoa.googleapi.service.GoogleMapApiService;
-import zzandori.zzanmoa.market.entity.Market;
-import zzandori.zzanmoa.market.repository.MarketRepository;
 import zzandori.zzanmoa.marketplace.dto.MarketPlaceResponseDto;
+import zzandori.zzanmoa.marketplace.dto.MarketPlaceReviewResponseDto;
 import zzandori.zzanmoa.marketplace.entity.MarketPlace;
+import zzandori.zzanmoa.marketplace.entity.MarketPlaceGoogleIds;
+import zzandori.zzanmoa.marketplace.repository.MarketPlaceGoogleIdsRepository;
 import zzandori.zzanmoa.marketplace.repository.MarketPlaceRepository;
 
 @RequiredArgsConstructor
@@ -20,6 +19,8 @@ import zzandori.zzanmoa.marketplace.repository.MarketPlaceRepository;
 public class MarketPlaceService {
 
     private final MarketPlaceRepository marketPlaceRepository;
+    private final MarketPlaceGoogleIdsRepository marketPlaceGoogleIdsRepository;
+    private final GoogleMapApiService googleMapApiService;
 
 
     public List<MarketPlaceResponseDto> getMarketPlaces() {
@@ -27,13 +28,27 @@ public class MarketPlaceService {
         return marketPlaces.stream()
             .filter(marketPlace -> marketPlace.getMarketAddress() != null)
             .map(
-            marketPlace -> MarketPlaceResponseDto.builder()
-                .marketId(marketPlace.getMarketId())
-                .marketName(marketPlace.getMarketName())
-                .latitude(marketPlace.getLatitude())
-                .longitude(marketPlace.getLongitude())
-                .build())
+                marketPlace -> MarketPlaceResponseDto.builder()
+                    .marketId(marketPlace.getMarketId())
+                    .marketName(marketPlace.getMarketName())
+                    .latitude(marketPlace.getLatitude())
+                    .longitude(marketPlace.getLongitude())
+                    .build())
             .collect(Collectors.toList());
+    }
+
+    public MarketPlaceReviewResponseDto getReviews(String marketId)
+        throws UnsupportedEncodingException {
+        Optional<String> placeId = marketPlaceGoogleIdsRepository.findByMarketId(marketId);
+        if (placeId.isPresent()) {
+            List<String> reviews = googleMapApiService.requestReview(placeId.get()).stream().map(review -> review.getText())
+                .toList();
+            return MarketPlaceReviewResponseDto.builder()
+                .reviews(reviews)
+                .build();
+        }
+
+        return null;
     }
 
 }
